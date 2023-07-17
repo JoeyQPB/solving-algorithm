58d2a6b01ded9469e222f615e265ed73f5811d53
joey <joey_qpb@hotmail.com>
2023-07-17 13:25:58 +1000

    feat: new class to get user data

---
+++ src/utils/GetData.java
@@ -0,0 +1,15 @@
+package utils;
+import java.util.Scanner;
+public class GetData {
+    public int[] array() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter the array length to be sorted: ");
+        int[] arr = new int[Integer.parseInt(sc.nextLine())];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+        return arr;
+    }
+}
