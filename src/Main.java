aff8149eb7473e213e9026eb2691c35d4ed244fc
joey <joey_qpb@hotmail.com>
2023-07-18 21:27:50 +1000

    feat: implements ExecutePick class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import algorithms.*;
 import utils.Start;
+import services.ExecutePick;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,31 +11,7 @@ public class Main {
 
         do {
             pick = start.pick();
-
-            switch (pick) {
-                case 1 -> {
-                    Fibonacci fibonacci = new Fibonacci();
-                    fibonacci.showResult();
-                }
-                case 2 -> {
-                    LinearSearch linearSearch = new LinearSearch();
-                    linearSearch.showResult();
-                }
-                case 3 -> {
-                    BinarySearch binarySearch = new BinarySearch();
-                    binarySearch.showResult();
-                }
-                case 4 -> {
-                    BubbleSort bubbleSort = new BubbleSort();
-                    bubbleSort.showResult();
-                }
-                case 5 -> {
-                    InsertionSort insertionSort = new InsertionSort();
-                    insertionSort.showResult();
-                }
-                default -> System.out.println("Invalid Option!");
-            }
-
+            new ExecutePick(pick);
             System.out.println("\nDid you wanna try again? (y/n)");
         } while (sc.nextLine().trim().equalsIgnoreCase("Y"));
 
