bcae1c0b0c74aaffeb710aeeefa94c63ae2fe241
joey <joey_qpb@hotmail.com>
2023-07-18 21:28:07 +1000

    feat: ExecutePick class

--- src/services/ExecutePick.java
+++ src/services/ExecutePick.java
@@ -7,29 +7,13 @@ public class ExecutePick {
     public ExecutePick(int pick) {
 
         switch (pick) {
-            case 1 -> {
-                Fibonacci fibonacci = new Fibonacci();
-                fibonacci.showResult();
-            }
-            case 2 -> {
-                LinearSearch linearSearch = new LinearSearch();
-                linearSearch.showResult();
-            }
-            case 3 -> {
-                BinarySearch binarySearch = new BinarySearch();
-                binarySearch.showResult();
-            }
-            case 4 -> {
-                BubbleSort bubbleSort = new BubbleSort();
-                bubbleSort.showResult();
-            }
-            case 5 -> {
-                InsertionSort insertionSort = new InsertionSort();
-                insertionSort.showResult();
-            }
-            case 6 -> {
-                System.out.println("PICK 6");
-            }
+            case 1 -> new Fibonacci();
+            case 2 -> new LinearSearch();
+            case 3 -> new BinarySearch();
+            case 4 -> new BubbleSort();
+            case 5 -> new InsertionSort();
+            case 6 -> new SelectionSort();
+
             default -> System.out.println("Invalid Option!");
         }
 
